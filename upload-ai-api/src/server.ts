d71337c79df9a699086b3ba08b470d06afebb2c6
Artur Rodrigues <artur190503@outlook.com>
2023-09-16 03:20:31 -0300

    Aula 2 finalizada

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,17 +1,23 @@
-import { fastify } from 'fastify'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { uploadVideoRoute } from './routes/upload-video'
-import { createTranscriptionRoute } from './routes/create-transcription'
-
+import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/gera-complemento-ia";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
-    port: 3333,
+  port: 3333
 }).then(() => {
-    console.log('Server is running on port 3333!')
+  console.log('HTTP Server Running!')
 })
